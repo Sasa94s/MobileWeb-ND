b19d21ddfafca8389f38945b4d30b967387e4255
Mostafa Elsheikh <sasa94s@msn.com>
2018-07-23 14:51:31 +0200

    modify for live version

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -22,7 +22,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
       L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-        mapboxToken: '<your MAPBOX API KEY HERE>',
+        mapboxToken: 'pk.eyJ1Ijoic2FzYTk0cyIsImEiOiJjampybGh1bHIyampuM2ttcDZmOGw5MDhhIn0.SgFa1GaIi_RWAfnjma3UGQ',
         maxZoom: 18,
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
           '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
